eef70dd6fa313857c55aa8283ef421a3cf7350c4
Petr Suk <petr.suk@clevermaps.io>
2023-02-26 18:10:10 +0100

    The list of CZ weather station;  URL for data download

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # zero_closed_temperature
 Few analysis based on the weather stations data with the focus on days with the temerature slightly above zero.
+
+## SRC data
+The list of weather stations and the links to the data comes from the CHMI website:
+https://www.chmi.cz/historicka-data/pocasi/denni-data/Denni-data-dle-z.-123-1998-Sb
+
+The problem is, that either the list of station nor the datasets are not directly downloadable. Developer consol (F12) must be used to find the URLs and get the download links.
+
---
+++ cz/src_weather_data/extract/get_weather_stations_data.py
@@ -0,0 +1,17 @@
+## prumerna teplota
+https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/T/Pardubicky/H2ZAMB01_T_N.csv.zip
+
+## maximalni teplota
+https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/TMA/Jihocesky/C1BLAT01_TMA_N.csv.zip
+
+## minimalni teplota
+https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/TMI/Praha/P1PKLE01_TMI_N.csv.zip
+
+## denni uhrn srazek
+https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/SRA/Praha/P1PCHO01_SRA_N.csv.zip
+
+## vyska nove napadleho snehu
+https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/SNO/Karlovarsky/L2TSEK01_SNO_N.csv.zip
+
+## celkova vyska snehove pokryvky
+https://www.chmi.cz/files/portal/docs/meteo/ok/open_data/RDATA/SCE/Vysocina/B2BRTN01_SCE_N.csv.zip
